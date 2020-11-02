6badccfbaca14c0bf4da9b1ae3e1cb17dd3d7e6c
BrunoFant <bruno@myneo.me>
2020-11-02 13:38:14 +0000

    Also fix the np.messages options as it seemed to have a typo

--- R/aide.R
+++ R/aide.R
@@ -184,7 +184,7 @@ main_one = function(cgene, bam_path, bamTotal, readLen, strandmode){
 #' @author Jingyi Jessica Li, \email{jli@stat.ucla.edu}
 aide = function(gtf_path, bam_path, fasta_path, out_dir, readLen, strandmode = 0, genes = NULL, pval = NULL, ncores = 5,
                 mode = "regular", ne = 25, gene_model_path = NULL, flag = 0){
-  options(np.messsages = FALSE)
+  options(np.messages = FALSE)
   set.seed(1)
 
   tp_dir = paste0(out_dir, "temporary/")
